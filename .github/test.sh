a99f6307e44a30d442509378dc74dc412d1faf50
Wooter <rick@astley.nevergonnagiveyouup>
2022-04-09 18:49:13 +0200

    hope this really works

--- .github/test.sh
+++ .github/test.sh
@@ -3,7 +3,7 @@
 
 ./singleprogram &
 netsurf about:blank &
-sleep 8; ffmpeg -f x11grab -i ${DISPLAY} -frames:v 1 output.png || killall -SIGKILL singleprogram; killall -SIGKILL netsurf; exit 1
+sleep 8; ffmpeg -f x11grab -i ${DISPLAY} -frames:v 1 output.png 
 sleep 1
 killall -SIGKILL singleprogram
 killall -SIGKILL netsurf || killall -SIGKILL netsurf; true
