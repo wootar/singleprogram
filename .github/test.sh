e51ce434ccff42f035b64c30a05aa5a18e13b554
Wooter <rick@astley.nevergonnagiveyouup>
2022-04-09 18:31:50 +0200

    Replace st with netsurf

--- .github/test.sh
+++ .github/test.sh
@@ -2,9 +2,9 @@
 # Test script for github actions
 
 ./singleprogram &
-st &
-sleep 2; ffmpeg -f x11grab -i ${DISPLAY} -frames:v 1 output.png || exit 1
+netsurf http://example.com &
+sleep 8; ffmpeg -f x11grab -i ${DISPLAY} -frames:v 1 output.png || killall -SIGKILL singleprogram; killall -SIGKILL netsurf; exit 1
 sleep 1
 killall -SIGKILL singleprogram
-killall -SIGKILL st || killall -SIGKILL stterm; true
+killall -SIGKILL netsurf || killall -SIGKILL netsurf; true
 exit 0
