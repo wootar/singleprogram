c0bfbc3c54eddc33cd437da485b00a8c0c027e49
Wooter <rick@astley.nevergonnagiveyouup>
2022-04-09 18:47:24 +0200

    Fix netsurf?

--- .github/test.sh
+++ .github/test.sh
@@ -2,7 +2,7 @@
 # Test script for github actions
 
 ./singleprogram &
-netsurf http://example.com &
+netsurf about:blank &
 sleep 8; ffmpeg -f x11grab -i ${DISPLAY} -frames:v 1 output.png || killall -SIGKILL singleprogram; killall -SIGKILL netsurf; exit 1
 sleep 1
 killall -SIGKILL singleprogram
